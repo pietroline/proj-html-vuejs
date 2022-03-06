2a3a5711ca9250a6372f81f17ae68c36e2a0192c
pietroline <pietrobarberi@hotmail.it>
2022-03-06 10:11:02 +0100

    refactoring header top

--- src/components/partials/BiggestEvent.vue
+++ src/components/partials/BiggestEvent.vue
@@ -2,18 +2,47 @@
   <section class="my-2 d-flex align-items-center justify-content-center">
 
         <span>Start TOMORROW! Our biggerest event of the year...</span>
-        <span class="mx-3 text-dark">
-            <i class="bi bi-clock me-2"></i>
-            <span>00:00:00:00</span>
+        <span class="mx-4 text-dark d-flex align-items-center">
+            <i class="bi bi-clock me-3 fs-4"></i>
+            <Countdown class="countdown" :deadline="deadline()"></Countdown>
         </span>
-        <button class="btn px-4 py-2">Get ticket</button>
-
+        <MyBtn :text="'Get ticket'"/>
   </section>
 </template>
 
 <script>
+
+    //importo dayjs e aggiungo estenzione customPardeFormat
+    const dayjs = require('dayjs')
+    var customParseFormat = require('dayjs/plugin/customParseFormat')
+    dayjs.extend(customParseFormat)
+
+    //importo componente Countdown
+    import Countdown from 'vuejs-countdown';
+
+    import MyBtn from './MyBtn.vue';
+    
     export default {
         name: "BiggestEvent",
+        components:{
+            Countdown,
+            MyBtn,
+        },
+        methods:{
+            deadline(){
+
+                //che giorno è oggi?
+                let data = dayjs().format();
+                 
+                //aggiungo 1 giorno a quella di oggi
+                data = dayjs(data).add(1, "d");
+
+                //converto la data in stringa Mese Giorno, Anno
+                data = dayjs(data).format("MMMM DD, YYYY")
+
+                return data;
+            }
+        }
         
     }
 </script>
---
+++ src/components/partials/MyBtn.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="btn px-4 py-2">{{text}}</button>
+</template>
+
+<script>
+    export default {
+        name: "MyBtn",
+        props:{
+            "text": String,
+        }
+    }
+</script>
+
+<style>
+
+</style>
